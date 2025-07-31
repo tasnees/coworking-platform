f2ea5ac5e4f8038164bc74bb7c5ebc5228ffa326
tasnees <graba.tasneem@gmail.com>
2025-07-31 14:53:15 +0100

    final fixes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type React from "react"
 import type { Metadata } from "next"
+import '../styles/globals.css';
 import "./globals.css"
 import { Toaster } from "@/components/ui/toaster"
 import { Providers } from "./providers"
