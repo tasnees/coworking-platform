192fa4a799fdbe84e47b045782174292f8470511
tasnees <graba.tasneem@gmail.com>
2025-09-04 13:20:01 +0100

    camisa negra

--- postcss.config.js
+++ postcss.config.js
@@ -1,20 +1,22 @@
 module.exports = {
   plugins: {
     'postcss-import': {},
-    'tailwindcss/nesting': {},
-    tailwindcss: {},
-    'postcss-nested': {},
-    autoprefixer: {},
+    '@tailwindcss/nesting': {},
+    'tailwindcss': {},
+    'autoprefixer': {},
     'postcss-flexbugs-fixes': {},
     'postcss-preset-env': {
       autoprefixer: {
-        flexbox: 'no-2009'
+        flexbox: 'no-2009',
+        grid: 'autoplace',
       },
       stage: 3,
       features: {
         'custom-properties': false,
-        'nesting-rules': true
-      }
-    }
-  }
+        'nesting-rules': true,
+        'custom-media-queries': true,
+        'color-mod-function': true,
+      },
+    },
+  },
 }
