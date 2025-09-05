84a0540cc4e6aa869e28afcd6d50d209e5c6c9a9
tasnees <graba.tasneem@gmail.com>
2025-09-05 23:07:18 +0100

    freeza

--- postcss.config.js
+++ postcss.config.js
@@ -1,22 +1,8 @@
 module.exports = {
   plugins: {
-    'postcss-import': require.resolve('postcss-import'),
+    'postcss-import': {},
     '@tailwindcss/nesting': {},
-    'tailwindcss': {},
-    'autoprefixer': {},
-    'postcss-flexbugs-fixes': {},
-    'postcss-preset-env': {
-      autoprefixer: {
-        flexbox: 'no-2009',
-        grid: 'autoplace',
-      },
-      stage: 3,
-      features: {
-        'custom-properties': false,
-        'nesting-rules': true,
-        'custom-media-queries': true,
-        'color-mod-function': true,
-      },
-    },
-  },
+    tailwindcss: {},
+    autoprefixer: {}
+  }
 }
